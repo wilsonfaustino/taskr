b102afecc341e6bd52a7686f903850481394a5a1
Wilson Faustino <wilson.faustino@syngenta.com>
2025-05-28 22:21:00 -0300

    feat: integrate authentication check with LoginScreen in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,14 @@
+'use client'
+
+import { LoginScreen } from '@/components/app/login-screen'
 import { Tasks } from '@/components/app/tasks'
+import { useAuthContext } from '@/core/context/auth-context'
 
 export default function Home() {
+  const { email } = useAuthContext()
+
+  if (!email) return <LoginScreen />
+
   return (
     <div className="h-screen w-full font-[family-name:var(--font-geist-sans)]">
       <Tasks />
